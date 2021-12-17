2d9e90f5f14ad30754c88afdb88c1be8128caa8f
BruceWayne222 <77908276+BruceWayne222@users.noreply.github.com>
2021-12-17 15:28:58 +0530

    Update strings.py

--- InlineBot/strings.py
+++ InlineBot/strings.py
@@ -13,9 +13,9 @@ from InlineBot import (
 if CUSTOM_START_MESSAGE:
     START_MESSAGE = CUSTOM_START_MESSAGE
 else:
-    START_MESSAGE = """<b>Hello {mention},
-
-I am an Inline Saver Bot, you can save inline filters and It can be use in any of your chats easily, Click help for more details</b> 
+    START_MESSAGE = """<b>Hello {mention}😊✌️,
+I'm MHSL filter bot🎬 
+</b> 
 """
 
 HELP_MESSAGE = f"""<b><u>Main Available Commands</u></b>
@@ -49,12 +49,12 @@ HELP_MESSAGE = f"""<b><u>Main Available Commands</u></b>
 
 ABOUT_MESSAGE = f"""<b><u>ABOUT ME</u></b>
 
-<b>○ Maintained by : <a href='tg://user?id={OWNER_ID}'>This Person</a>
-○ Channel : <a href='https://t.me/CodeXBotz'>Code 𝕏 Botz</a>
-○ Support : <a href='https://t.me/CodeXBotzSupport'>Code 𝕏 Botz Support</a>
-○ Source Code : <a href='https://github.com/CodeXBotz/Inline-Filter-Bot'>Click here</a>
+<b>○ Maintained by : <a href='tg://user?id={OWNER_ID}'>Jason Todd</a>
+○ Channel : <a href='https://t.me/MovieHubSLC'>🎬 Movie Hub SL🎬</a>
+○ Support : <a href='https://t.me/MHSLsupport'> Support</a>
+
 ○ Language : <a href='https://www.python.org/'>Python 3</a>
-○ Library : <a href='https://github.com/pyrogram/pyrogram'>Pyrogram Asyncio {__version__}</a></b>
+○ Library : Pyrogram Asyncio {__version__}</a></b>
 """
 
 MARKDOWN_HELP = """<b><u>Markdown Formatting</u></b>
